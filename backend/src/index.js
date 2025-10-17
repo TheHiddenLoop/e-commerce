88a612429515e7b318f48fab624369b400c54d0a
Angesh Chauhan <chauhanangesh60@gmail.com>
2025-10-17 14:04:08 +0530

    re deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,10 +41,10 @@ app.use("/api/v1/admin",  adminRouter);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*splat", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
