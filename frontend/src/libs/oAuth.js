f063ebf3d2ba97f49afe47c7b68a0f49563475c5
Angesh Chauhan <chauhanangesh60@gmail.com>
2025-10-18 13:55:38 +0530

    re deploy

--- frontend/src/libs/oAuth.js
+++ frontend/src/libs/oAuth.js
@@ -1,7 +1,7 @@
 export const googleOAuth = () => {
-  window.location.href = "https://e-commerce-ewon.onrender.com/api/v1/auth/google";
+  window.location.href = "https://e-commerce-2-0r1h.onrender.com/api/v1/auth/google";
 };
 
 export const facebook = () => {
-    window.open("https://e-commerce-ewon.onrender.com/auth/facebook", "_self");
+    window.open("https://e-commerce-2-0r1h.onrender.com/auth/facebook", "_self");
   };
