db88fa5510d9bd10cd669021323a1d25945ad514
Angesh Chauhan <chauhanangesh60@gmail.com>
2025-10-18 10:28:11 +0530

    re deploy

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "https://e-commerce-1-txaf.onrender.com/api/v1",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "/api",
   withCredentials: true,
 });
