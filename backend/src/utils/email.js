e014ca926499b4c637109bba43680a23ca4ad193
Angesh Chauhan <chauhanangesh60@gmail.com>
2025-10-21 18:14:24 +0530

    re deploy

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -1,13 +1,17 @@
 import nodemailer from "nodemailer";
 
-export const sendOTPEmail=async(to, subject, html)=>{
+export const sendOTPEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASS,
     },
+    connectionTimeout: 20000,
   });
+
   await transporter.sendMail({
     from: `"Auth Support" <${process.env.GMAIL_USER}>`,
     to,
