05cb14ec4e51dc3f676214b113a86de6def86695
Angesh Chauhan <chauhanangesh60@gmail.com>
2025-11-07 19:52:45 +0530

    debug

--- frontend/src/libs/oAuth.js
+++ frontend/src/libs/oAuth.js
@@ -1,7 +1,7 @@
 export const googleOAuth = () => {
-  window.location.href = "https://e-commerce-2-0r1h.onrender.com/api/v1/auth/google";
+  window.location.href = "https://e-cltxbackend.vercel.app/api/v1/auth/google";
 };
 
 export const facebook = () => {
-    window.open("https://e-commerce-2-0r1h.onrender.com/api/v1/auth/facebook", "_self");
+    window.open("https://e-cltxbackend.vercel.app/api/v1/auth/facebook", "_self");
   };
